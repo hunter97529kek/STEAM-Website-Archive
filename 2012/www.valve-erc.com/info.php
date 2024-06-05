beb51fca10bdb8f5b400063023c68f7e7f8113d3
hunt <flyinggun@seznam.cz>
2024-06-05 10:43:48 +0100

    2012 valve-erc

---
+++ 2012/www.valve-erc.com/info.php
@@ -0,0 +1,53 @@
+
+<html>
+<title>Information for valve-erc.com</title>
+<body bgcolor='white'>
+<center>
+<table border='0' width='800'>
+	<tr>
+		<td colspan='2' align='left'>
+			<div style='width: 800px; border: 1px solid #E8E8E8; padding: 5px; text-align:left;'>
+				<div style="background-image: url('/welcome.gif'); display:block; width: 381px; height: 190px;">
+					<table border='0' width='381'>
+						<tr>
+							<td width='180'>
+								&nbsp;
+							</td>
+							<td width='201' height='185' align='center' valign='middle'>
+								valve-erc.com 
+							</td>
+						</tr>
+					</table>
+				</div>
+ 			</div>
+		</td>
+	</tr>
+	<tr>
+		<td width='800' valign='middle' colspan='2' align='center'>
+			<div style='width: 800px; border: 1px solid #C8C8C8; background-color: #E2E2E2; text-align:center; height: 2px;'>
+				<nobr>
+			</div>
+		</td>
+	</tr>
+	<tr>
+		<td width='800' valign='top' align='left'>
+			<div style='width: 100%; height: 100px; border: 1px solid #C8C8C8; background-color: #FEFEFE; text-align:center; padding-top: 10px; font-family:arial; font-size: 12px;'>
+				<br>
+				This domain name has expired.<br><br> To renew this domain please visit<br>
+				Domain Name Systems at <a href="http://www.domainnamesystems.com">http://www.domainnamesystems.com</a> 			</div>
+			<br>
+		</td>
+	</tr>
+</table>
+<hr width='800' color='#DEDEDE'>
+<table border='0' width='800'>
+	<tr>
+		<td width='100%' align='right' style='font-size: 11px;'>
+			Copyright &copy; 2008. All Rights Reserved.<br><a href='http://www.cheapies.com'>Cheapies.com</a>
+		</td>
+	</tr>
+</table>
+<br>
+</center>
+</body>
+</html>
